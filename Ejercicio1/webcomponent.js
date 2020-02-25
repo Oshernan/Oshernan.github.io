c3a58982029c3e23d1e3279e6ab00ddecb0737b7
Oshernan <o.racso11@gmail.com>
2020-02-25 10:28:42 +0100

    Update webcomponent.js

--- Ejercicio1/webcomponent.js
+++ Ejercicio1/webcomponent.js
@@ -12,9 +12,6 @@
             this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._firstConnection = false;
-            this._tagContainer;
-            this._tagType = "h1";
-            this._tagText = "Hello World";
         }
 
         //Fired when the widget is added to the html DOM of the page
