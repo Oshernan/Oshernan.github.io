41135d1965964c13b13f903fa0df51deb9904083
Oshernan <o.racso11@gmail.com>
2020-02-25 11:21:38 +0100

    Update

--- Ejercicio1/webcomponent.js
+++ Ejercicio1/webcomponent.js
@@ -4,21 +4,17 @@
         <h1>Hello World</h1>
     `;
 
-    customElements.define('com-sap-sample-helloworld2', class HelloWorld2 extends HTMLElement {
+    customElements.define('com-sap-sample-helloworld2', class HelloWorld1 extends HTMLElement {
 
 
         constructor() {
             super(); 
             this._shadowRoot = this.attachShadow({mode: "open"});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._firstConnection = false;
-
         }
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            this._firstConnection = true;
-            this.redraw();
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
@@ -33,6 +29,7 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
         onCustomWidgetAfterUpdate(oChangedProperties) {
+            this.redraw();
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
@@ -41,15 +38,12 @@
 
         
         //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+        // Commented out by default
         /*
         onCustomWidgetResize(width, height){
-        
         }
         */
 
-        redraw(){
-
+        redraw(){}
     });
 })();
